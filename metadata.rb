c8c5984c13585656f0997bed2f1e28933bc15582
Mike Stallard <mstallard@pivotal.io>
2016-05-03 15:23:52 -0700

    Add metadata.rb
    Signed-off-by: Heather Farley <hrfarley@gmail.com>

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'sprout-iad'
+maintainer       'Pivotal IAD'
+maintainer_email 'iad-dev@pivotal.io'
+license          'MIT'
+description      'Installs/Configures things the IAD team likes'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+supports         'mac_os_x'
