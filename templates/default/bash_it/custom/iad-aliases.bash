25ae9c73340e19a906103b3529440877df835fa6
Mike Stallard <mstallard@pivotal.io>
2016-05-04 09:28:56 -0700

    Rename update task
    Signed-off-by: Heather Farley <hrfarley@gmail.com>

--- templates/default/bash_it/custom/iad-aliases.bash
+++ templates/default/bash_it/custom/iad-aliases.bash
@@ -1,4 +1,4 @@
 alias resprout='(cd ~/workspace/sprout-wrap && git pull && rbenv exec bundle exec soloist)'
 alias rebrew='brew update && brew upgrade'
-alias update='resprout && rebrew'
+alias update_all='resprout && rebrew'
 
