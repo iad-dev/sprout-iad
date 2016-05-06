3c188cb57d9b7747bcce69f6927ab3779d72cef7
Sidney Ho <sid@sidho.me>
2016-05-06 12:18:37 -0700

    Update resprout to work with rbenv
    Signed-off-by: Sidney Ho <sid@sidho.me>

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'iad-dev@pivotal.io'
 license          'MIT'
 description      'Installs/Configures things the IAD team likes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 supports         'mac_os_x'
