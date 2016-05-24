ed5f4ee3fc27c031791b96f8d1096f3d767f1a8f
Heather Farley <hrfarley@gmail.com>
2016-05-24 10:35:49 -0700

    add invoker for running meeple apps
    Signed-off-by: Ian Fisher <loktar@gmail.com>

---
+++ recipes/invoker.rb
@@ -0,0 +1,55 @@
+RUBY_VERSION = '2.3.0'
+
+# Run setup to get .dev domains
+bash 'invoker-install-gems' do
+  code <<-BASH
+    set -e
+
+    sudo -s "chruby-exec #{RUBY_VERSION} -- bash -c 'gem install invoker'"
+    chown pivotal:staff /Users/pivotal/.rubies/ruby-#{RUBY_VERSION}/bin/invoker
+    chruby-exec #{RUBY_VERSION} -- gem install terminal-notifier
+  BASH
+end
+
+bash 'invoker-setup' do
+  code "sudo -s 'chruby-exec #{RUBY_VERSION} -- invoker setup'"
+end
+
+# Ini file
+file "#{ENV['HOME']}/.invoker/iad-apps.ini" do
+  action :create
+  content <<-INI
+[meeple-config]
+directory = ~/workspace/meeple-config
+command = rake clean build default
+port = 8081
+
+[meeple-auth]
+directory = ~/workspace/meeple-auth
+command = rake clean build default
+port = 8082
+
+[meeple-people]
+directory = ~/workspace/meeple-people
+command = rake clean build default
+port = 8083
+
+[allocations]
+directory = ~/workspace/allocations
+command = foreman start
+port = 4000
+disable_autorun = true
+
+[pivots]
+directory = ~/workspace/pivots-two
+command = bundle exec foreman start -f Procfile.dev
+port = 4010
+disable_autorun = true
+
+[augur]
+directory = ~/workspace/augur
+command = bundle exec foreman start -f Procfile.dev
+port = 4020
+disable_autorun = true
+  INI
+end
