fa45133ccc917093cc731b857043d85ea22c0891
Molly T-M <molly.kayline@gmail.com>
2016-08-10 10:43:12 -0700

    Add JAVA_HOME to env vars
    [finishes #128085133]
    
    Signed-off-by: Max Gordon <gordonmaxc@gmail.com>

---
+++ templates/default/bash_it/custom/env-variables.bash
@@ -0,0 +1,6 @@
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+
+#Find Meeple Config values in LastPass
+export MEEPLE_CONFIG_GIT_USERNAME=
+export MEEPLE_CONFIG_GIT_PASSWORD=
+export MEEPLE_CONFIG_SECRET=
