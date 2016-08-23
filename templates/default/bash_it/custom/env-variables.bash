8fab6374e757452aa95bc86540ca444d477ca031
Mike Stallard <mstallard@pivotal.io>
2016-08-23 16:42:07 -0700

    Add system ruby gems to PATH
    Signed-off-by: Max Gordon <gordonmaxc@gmail.com>

--- templates/default/bash_it/custom/env-variables.bash
+++ templates/default/bash_it/custom/env-variables.bash
@@ -1,6 +1,8 @@
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+export PATH=$PATH:/Users/pivotal/.gem/ruby/2.0.0/bin/
 
 #Find Meeple Config values in LastPass
 export MEEPLE_CONFIG_GIT_USERNAME=
 export MEEPLE_CONFIG_GIT_PASSWORD=
 export MEEPLE_CONFIG_SECRET=
+
