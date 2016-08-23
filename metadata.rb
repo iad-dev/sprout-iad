eca294a43ef1b58f39ef237239104318e5848976
iad-dev <iad-dev@pivotal.io>
2016-08-23 16:02:33 -0700

    Bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'iad-dev@pivotal.io'
 license          'MIT'
 description      'Installs/Configures things the IAD team likes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.9'
 supports         'mac_os_x'
