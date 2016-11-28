99eba65dcf358a537acaddd75d1c9795c7c878bc
Elana Koren <ekoren@pivotal.io>
2016-11-28 10:52:44 -0800

    Add a recipe to create a pivotal mysql user [#131108081]

---
+++ recipes/mysql-pivotal-user.rb
@@ -0,0 +1,11 @@
+ROOT_PASSWORD = node['sprout']['mysql']['root_password']
+USERNAME = 'foobar'
+
+execute 'create a pivotal user with no password' do
+  command "mysql -uroot -p#{ROOT_PASSWORD} -e \"create user '#{USERNAME}' identified by ''\""
+  not_if "mysql -u#{USERNAME} -p -e 'show databases'"
+end
+
+execute 'give the pivotal user root permissions' do
+  command "mysql -uroot -p#{ROOT_PASSWORD} -e \"grant all privileges on *.* to '#{USERNAME}'\""
+end
