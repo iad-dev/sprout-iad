de94e6715dee28af25dd80f927fc41d9fcee0e45
Mike Stallard <mstallard@pivotal.io>
2017-03-06 10:39:28 -0800

    Add meeple cli alias
    This should eventually move to a meeple cookbook, but this is the easiest thing for now.
    
    Signed-off-by: Whitney Schaefer <wschaefer@pivotal.io>

--- templates/default/bash_it/custom/iad-aliases.bash
+++ templates/default/bash_it/custom/iad-aliases.bash
@@ -1,4 +1,4 @@
 alias resprout='(cd ~/workspace/sprout-wrap && git pull && rbenv exec bundle exec soloist)'
 alias rebrew='brew update && brew upgrade'
 alias update_all='resprout && rebrew'
-
+alias meeple='/Users/pivotal/workspace/meeple/cli/bin/meeple'
